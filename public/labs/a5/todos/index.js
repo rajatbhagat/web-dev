2f6a76a045d7c63a1de4b6d3f0d3b8d101938316
rajatbhagat <rajat.bhagat@gmail.com>
2022-02-24 13:01:24 -0500

    Lab and TodoList for A5

---
+++ public/labs/a5/todos/index.js
@@ -0,0 +1,10 @@
+import TodoList from './ToDoList.js'
+alert('Hello World!');
+
+$('#wd-todo').append(`
+    <div class="container">
+        <h1>Todo example</h1>
+        ${TodoList()}
+    </div>
+`);
+
