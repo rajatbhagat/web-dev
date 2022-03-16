b77a68ee8977b4ce22129aa139afef6c02df5e6e
rajatbhagat <rajat.bhagat@gmail.com>
2022-03-16 17:55:25 -0400

    Completed labs

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import HelloWord from "./components/hello-world";
+import Labs from "./components/labs";
+import Tuiter from "./components/tuiter";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-      <div className="container">
-        <HelloWord />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+            <Routes>
+                <Route path="/hello" element={<HelloWord />}/>
+                <Route path="/" element={<Labs/>}/>
+                <Route path="/tuiter" element={<Tuiter/>}/>
+            </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const HelloWord = () => {
     return (
-        <h1>Hello World</h1>
-    );
+        <>
+            <h1>Hello World</h1>
+            <Link to="/">
+                Labs
+            </Link> |
+            <Link to="/tuiter">
+                Tuiter
+            </Link>
+        </>
+);
 }
 
 export default HelloWord;
