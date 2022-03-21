6424ab61a2075026e204e97ced53f465dc4392c3
Rajat Bhagat <rajat.bhagat@gmail.com>
2022-03-21 11:06:15 -0400

    Updating routes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 import HelloWord from "./components/hello-world";
 import Labs from "./components/labs";
-import Tuiter from "./components/tuiter";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import HomeScreen from "./components/tuiter/HomeScreen/HomeScreen";
 import ExploreScreen from "./components/tuiter/ExploreScreen/ExploreScreen";
 
@@ -12,6 +11,11 @@ function App() {
   return (
       <BrowserRouter>
         <div className="container-fluid">
+            <Link to="/hello"/>
+            <Link to="/"/>
+            <Link to="/tuiter"/>
+            <Link to="/tuiter/home"/>
+            <Link to="/tuiter/explore"/>
             <Routes>
                 <Route path="/hello" element={<HelloWord />}/>
                 <Route path="/" element={<Labs/>}/>
