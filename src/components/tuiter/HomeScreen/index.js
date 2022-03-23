922d69d7158e58e8e5598c46da843ec0479cbe4d
Rajat Bhagat <rajat.bhagat@gmail.com>
2022-03-23 19:25:34 -0400

    Adding the ability to create new tuits

--- src/components/tuiter/HomeScreen/index.js
+++ src/components/tuiter/HomeScreen/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import TuitList from "../tuit-list";
+import WhatsHappening from "../whatsHappening/whats-happening";
 
 const HomeScreen = () => {
     return(
         <div>
+            <WhatsHappening />
             <TuitList />
         </div>
     )
--- src/components/tuiter/reducers/tuit-reducer.js
+++ src/components/tuiter/reducers/tuit-reducer.js
@@ -1,7 +1,25 @@
 import tuits from "../data/tuits.json";
 
-const tuitsReducer = (state = tuits) => {
-    return state;
+const tuitsReducer = (state = tuits, action) => {
+    switch (action.type) {
+        case 'CREATE_TUIT':
+            const newTuit = {
+                tuit: action.tuit,
+                _id: (new Date()).getTime() + '',
+                postedBy: {
+                    "username": "ReactJS"
+                },
+                stats: {
+                    retuits: 111,
+                    likes: 222,
+                    replies: 333
+                }
+            }
+            return [newTuit,
+                ...tuits];
+        default:
+            return state;
+    }
 }
 
 export default tuitsReducer;
